4f300a9e5c44da3f0d18018dbed76c67af3d1e4e
tjdcjf1996 <r_louis@naver.com>
2024-10-03 21:34:54 +0900

    스테이지 구분 구현
    서버 측 데이터 테이블 조회 API 구현
    클라이언트 측 데이터 테이블 fetch 구현
    
    데이터 테이블에 따른 스테이지 변동 구현

--- public/Score.js
+++ public/Score.js
@@ -1,6 +1,20 @@
 import { sendEvent } from "./Socket.js";
 
+let stageScore;
+async function fetchData() {
+  try {
+    const res = await fetch("/api/getAssets");
+    const data = await res.json();
+    stageScore = data.stage.data;
+  } catch (error) {
+    console.error("Error fetch:", error);
+  }
+}
+
+fetchData();
+
 class Score {
+  stage = 0;
   score = 0;
   HIGH_SCORE_KEY = "highScore";
   stageChange = true;
@@ -13,10 +27,26 @@ class Score {
 
   update(deltaTime) {
     this.score += deltaTime * 0.001;
-    // 점수가 100점 이상이 될 시 서버에 메세지 전송
-    if (Math.floor(this.score) === 100 && this.stageChange) {
-      this.stageChange = false;
-      sendEvent(11, { currentStage: 1000, targetStage: 1001 });
+    // 등록된 스테이지 데이터 테이블 점수 내에서만 구동
+    if (Math.floor(this.score) <= stageScore[stageScore.length - 1].score) {
+      // 다음 스테이지 조건 점수와 현재 점수가 같으면서 stageChance가 true일 때 스테이지 변경 시도
+      if (
+        Math.floor(this.score) === stageScore[this.stage + 1].score &&
+        this.stageChange
+      ) {
+        // 프레임단위 호출이기 때문에 여러번 호출되는 것을 방지
+        this.stageChange = false;
+        // 현재 스테이지 아이디와 타겟 스테이지 아이디를 서버로 전송함
+        sendEvent(11, {
+          currentStage: stageScore[this.stage].id,
+          targetStage: stageScore[this.stage + 1].id,
+        });
+        // 해당 점수가 지나갈 때 stageChange가 true로 바뀌게 1초 setTimeOut 설정
+        setTimeout(() => {
+          this.stage++;
+          this.stageChange = true;
+        }, 1000);
+      }
     }
   }
 
@@ -26,6 +56,7 @@ class Score {
 
   reset() {
     this.score = 0;
+    this.stage = 0;
     this.stageChange = true;
   }
 
--- src/handlers/stage.handler.js
+++ src/handlers/stage.handler.js
@@ -3,12 +3,16 @@ import { getStage, setStage } from "../models/stage.model.js";
 
 export const moveStageHandler = (userId, payload) => {
   let currentStage = getStage(userId);
+  const { stage } = getGameAssets();
 
+  // 유저가 진행한 스테이지가 없을 경우
   if (!currentStage.length) {
     return { status: "fail", message: "No stages found for user" };
   }
 
+  // 최근 진행한 스테이지를 오름차순으로 정렬
   currentStage.sort((a, b) => a.id - b.id);
+  // 가장 맨 뒤의 요소를 가져옴으로써 최근 스테이지를 찾음
   const currentStageId = currentStage[currentStage.length - 1].id;
   if (currentStageId !== payload.currentStage)
     return { status: "fail", message: "current stage mismatch" };
@@ -17,18 +21,25 @@ export const moveStageHandler = (userId, payload) => {
   const serverTime = Date.now();
   const elapsedTime =
     (serverTime - currentStage[currentStage.length - 1].timestamp) / 1000;
-  console.log(currentStage.timestamp);
-  console.log(elapsedTime);
-  // 1스테이지 -> 2스테이지로 넘어가는 가정
-  if (elapsedTime < 100 || elapsedTime > 105) {
+
+  // 기존 스테이지 점수를 구함
+  const curStage = stage.data.find((stage) => stage.id === currentStageId);
+
+  // 다음 타켓 스테이지의 점수를 구함.
+  const targetStage = stage.data.find(
+    (stage) => stage.id === payload.targetStage
+  );
+  if (!targetStage)
+    return { status: "fail", message: "Target stage not found" };
+  // 타겟 스테이지 점수와 기존 스테이지 점수의 차이를 구함.
+  const intervalScore = targetStage.score - curStage.score;
+
+  // 타겟 스테이지 점수 네트워크 시간 오차범위
+  if (elapsedTime < intervalScore - 0.5 || elapsedTime > intervalScore + 0.5) {
     return { status: "fail", message: "Invalid elapsed time" };
   }
 
-  const { stage } = getGameAssets();
-  if (!stage.data.some((stage) => stage.id === payload.targetStage))
-    return { status: "fail", message: "Target stage not found" };
-
   setStage(userId, payload.targetStage, serverTime);
 
-  return { status: "success" };
+  return { status: "success", message: "Move to target stage" };
 };
