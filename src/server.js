dd22446e73607e41c26cc863369921fd39efd15d
tjdcjf1996 <r_louis@naver.com>
2024-10-03 21:31:57 +0900

    assets api 추가
    스테이지 asset 조회 api 추가

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
-import express from "express";
+import express, { json } from "express";
 import { createServer } from "http";
 import initSocket from "./init/socket.js";
 import { loadGameAssets } from "./init/assets.js";
+import { getGameAssets } from "../src/init/assets.js";
 
 const app = express();
 const server = createServer(app);
@@ -12,6 +13,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
 initSocket(server); // 소켓 추가
+
+app.get("/api/getAssets", (req, res) => {
+  const data = getGameAssets();
+  if (!data) return res.status(404).json({ message: "Not found assets" });
+  return res.json(data);
+});
 
 server.listen(PORT, async () => {
   console.log(`Server is running on port ${PORT}`);
